845721fad5eb7082bda7e70254113010bcc16440
dvbnrg <dvbnrg@gmail.com>
2017-04-12 08:53:47 -0500

    Initial commit

---
+++ vueapp1/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+table {
+    width: 100%;
+}
+
+th, td {
+    padding: 15px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
---
+++ vueapp1/src/components/Hello.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+<table id="Tardis">
+  <th v-for="column in gridColumns">
+    {{column}}
+  </th>
+  <tr v-for="data in gridData">
+    <td>{{data.first_name}}</td>
+    <td>{{data.last_name}}</td>
+    <td>{{data.home}}</td>
+  </tr>
+</table>
+
+
+
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'The Vue.js App is working!',
+      input_val: '',
+      counter: 0,
+      searchQuery: '',
+      gridColumns: ['first_name', 'last_name','home'],
+      gridData: [ { first_name : 'Rose',
+                     last_name  : 'Tyler',
+                     home       : 'Earth' },
+                   { first_name : 'Zoe',
+                     last_name  : 'Heriot',
+                     home       : 'Space Station W3'},
+                   { first_name : 'Jo',
+                     last_name  : 'Grant',
+                     home       : 'Earth'},
+                   { first_name : 'Leela',
+                     last_name  : null,
+                     home       : 'Unspecified'},
+                   { first_name : 'Romana',
+                     last_name  : null,
+                     home       : 'Gallifrey'},
+                   { first_name : 'Clara',
+                     last_name  : 'Oswald',
+                     home       : 'Earth'},
+                   { first_name : 'Adric',
+                     last_name  : null,
+                     home       : 'Alzarius'},
+                   { first_name : 'Susan',
+                     last_name  : 'Foreman',
+                     home       : 'Gallifrey'} ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
